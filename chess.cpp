ccb55ed3b88d643c497ade103eff6d5515d03439
WetRock <70438953+WetRock@users.noreply.github.com>
2023-07-31 05:50:39 +0000

    Corrected the class so that they can be implemted

--- chess.cpp
+++ chess.cpp
@@ -2,10 +2,37 @@
 #include <iostream>
 using namespace std;
 
-board::board(/* args */)
+
+Piece::Piece(std::array<int, 2> position, bool white, int value, std::vector<std::vector<int>> moves)
+:position_(position), moves_(moves), value_(value), white_(white)
 {
+ 
 }
 
-board::~board()
+std::array<int, 2> Piece::get_position() {
+  return position_;
+}
+
+bool Piece::get_white() {
+  return white_;
+}
+
+std::vector<std::vector<int>> Piece::get_moves() {
+  return moves_;
+}
+
+void Piece::set_position(std::array<int, 2> position) {
+  position_ = position;
+}
+
+Board::Board() {
+  for (int i = 0; i < 8; ++i) {
+    for (int j = 0; j < 8; ++j) {
+      chessboard[i][j] = nullptr;
+    }
+  }
+}
+
+Board::~Board()
 {
 }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-
 #include "chess.h"
-using namespace std;
 
 int main() {
-    int pawnPosition[2] = {2, 1}; 
+    std::array<int, 2> pawnPosition = {2, 1}; 
     bool isWhitePawn = true;
     Pawn pawn(pawnPosition, isWhitePawn);
+    
+    std::cout << pawn.get_moves()[1][1];
     return 0;
 }
